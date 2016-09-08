652afb06b4869dc95926cd61dab0322ed76da2ca
jiongjiongwang <wjsnh@aliyun.com>
2016-09-08 20:39:05 +0800

    (1)CocoPods项目搭建完成。。

---
+++ JXBGJJ_iOS_App/BanGongJiaJu/BanGongJiaJu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BanGongJiaJu
+//
+//  Created by 王炯 on 16/9/8.
+//  Copyright © 2016年 王炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
